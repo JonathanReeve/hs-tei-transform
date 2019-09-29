016fa5b50e33ed62f5799027918b33025171417f
Jonathan Reeve <jon.reeve@gmail.com>
2019-09-29 13:58:38 -0400

    add test data and recent changes

--- Main.hs
+++ Main.hs
@@ -4,19 +4,21 @@
 module Main where
 
 import           Control.Monad                        (join)
+import           Control.Monad.Trans.Class            (lift)
 import           Control.Applicative                  ((<$>))
 import           Controllers.Home                     (home, login, post)
 import qualified Data.Map as M
 import           Data.Maybe                           (fromMaybe)
-import           Data.Text                            (Text)
+import           Data.Text                            (Text, concat)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import           Network.Wai.Middleware.Static        (addBase, noDots,
                                                        staticPolicy, (>->))
-import           Prelude hiding                       (readFile, writeFile)
+import           Prelude
 import           System.Environment                   (lookupEnv)
 import           Text.Blaze.Html5                     (Html, pre, toHtml)
 import           Text.Read                            (readMaybe)
 import           Text.XML
+import           Text.XML.Cursor
 import           Web.Scotty                           (middleware, scotty)
 
 main :: IO ()
@@ -26,7 +28,15 @@ main = do
         . fmap readMaybe <$> lookupEnv "PORT"
   -- readFile will throw any parse errors as runtime exceptions
   -- def uses the default settings
-  Document prologue root epilogue <- readFile def "01_sis.xml"
+
+  doc <- Text.XML.readFile def "header.xml"
+  let cursor = fromDocument doc
+      filenames = cursor $// laxElement "include" >=> attribute "href"
+  print filenames
+
+  -- TODO: load and parse all child files
+
+  Document prologue root epilogue <- Text.XML.readFile def "header.xml"
   let root' = transform root
       asText = renderText def (Document prologue root' epilogue)
       rendered = toHtml asText
@@ -46,19 +56,18 @@ goNode (NodeContent t) = [NodeContent t]
 goNode (NodeComment _) = [] -- hide comments
 goNode (NodeInstruction _) = [] -- and hide processing instructions too
 
--- Convert tag strings to formal names
--- Assumes TEI namespace.
-
--- convert each source element to its XHTML equivalent
+-- Convert each source element to its XHTML equivalent
 goElem :: Element -> Element
 goElem (Element name attrs children) =
   case (nameLocalName name) of
+    -- Comment out metadata in header
+    "teiHeader" -> Element "div" (hidden attrs) transformedChildren
     "lg" -> Element "div" lgAttrs transformedChildren
     "l" -> Element "span" M.empty transformedChildren
     "head" -> Element "h1" M.empty transformedChildren
-    "teiHeader" -> Element "div" (hidden attrs) transformedChildren
     "lb" -> Element "span" (lbAttrs attrs) (lbChildren attrs)
     "said" -> Element "span" (saidAttrs attrs) (saidChildren attrs children)
+    "name" -> spanIt (nameLocalName name) attrs children
     otherwise -> Element name attrs transformedChildren
   where
     transformedChildren = concatMap goNode children
@@ -70,10 +79,18 @@ goElem (Element name attrs children) =
     saidChildren attrs children = whoTag : transformedChildren
       where
         whoTag = NodeElement $ Element "span" (setClass "attribution") [NodeContent (attrs M.! "who")]
-    spanIt = Element "span" 
 
+-- processXinclude attrs = do
+--   Document prologue root epilogue <- Text.XML.readFile def (attrs M.! "href" :: FilePath)
+--   return root
+
+-- Make a Data.Map that looks like [("class":: Name, "lineNum")]
+-- So it can be used as an attribute. 
 mkAttrs :: Name -> Text -> M.Map Name Text
 mkAttrs key val = M.fromList [(key :: Name, val)]
 
+-- A wrapper for mkAttrs that can be used to set the HTML class. 
 setClass :: Text -> M.Map Name Text
 setClass val = mkAttrs "class" val
+
+spanIt name attrs children = Element "span" (M.insert "class" name attrs) children
